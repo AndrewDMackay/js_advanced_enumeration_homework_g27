796f7faae9e7770ba4f4166c2d67e6f2ce86ecff
Andrew Mackay <andrewmackayd@gmail.com>
2021-10-06 15:53:35 +0100

    3rd commit main

--- hw_advanced_start/pangram_finder/pangram_finder.js
+++ hw_advanced_start/pangram_finder/pangram_finder.js
@@ -1,9 +1,23 @@
+
 const PangramFinder = function (phrase) {
+  this.phrase = phrase.toLowerCase();
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
 }
 
-PangramFinder.prototype.isPangram = function () {
 
+// Pangram Finder..
+
+// A pangram is a sentence or phrase which contains every letter of the alphabet. 
+
+// "The quick brown fox jumps over the lazy dog." is probably the most notable pangram in English.
+
+// Given a sentence or phrase you should be able to determine whether or not every letter of the alphabet is included in it.
+
+
+PangramFinder.prototype.isPangram = function () {
+  return this.alphabet.every(character => this.phrase.includes(character));
 }
 
+
 module.exports = PangramFinder;
+
--- hw_advanced_start/upper_caser/upper_caser.js
+++ hw_advanced_start/upper_caser/upper_caser.js
@@ -1,9 +1,17 @@
 
 const UpperCaser = function (words) {
+    this.words = words;
 }
+
+
+// Uppercaser..
+
+// map() an array of strings to a new array containing uppercase versions of each string.
+
 
 UpperCaser.prototype.toUpperCase = function () {
-
+    return this.words.map(word => word.toUpperCase());
 }
+
 
 module.exports = UpperCaser;
